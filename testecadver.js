cf7ab49cf04d676cf58e47390206f51861d60979
João Fernandes Santos Filho <129438025+Fernandes072@users.noreply.github.com>
2023-06-13 19:08:48 -0300

    Add files via upload

--- testecadver.js
+++ testecadver.js
@@ -5,8 +5,6 @@ const port =  3001; // obter a porta do Vercel ou usar a porta 3000
 
 const app = express();
 
-app.use(express.static('public'));
-
 // Configura o middleware body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -41,7 +39,7 @@ app.post('/enviar-dados', function(req, res) {
   connection.query(sql, [nome, matricula, cpf, email, telefone, curso, turma, msg], function(err, result) {
     if (err) throw err;
     console.log('Dados inseridos com sucesso!');
-    res.send('Dados inseridos com sucesso!');
+    res.redirect('https://atestados.vercel.app/');
   });
 });
 
