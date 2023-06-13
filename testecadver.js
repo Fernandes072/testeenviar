700ff39270ce7848632ee9cb7799f61ec15d10f6
João Fernandes Santos Filho <129438025+Fernandes072@users.noreply.github.com>
2023-06-13 13:28:42 -0300

    Add files via upload

---
+++ testecadver.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000; // obter a porta do Vercel ou usar a porta 3000
+
+const app = express();
+
+app.use(express.static('public'));
+
+// Configura o middleware body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Cria a conexão com o banco de dados
+const connection = mysql.createConnection({
+    host: 'containers-us-west-190.railway.app',
+    user: 'root',
+    password: 'uF1YkyevPV5usJbw8iYn',
+    database: 'railway',
+    port: '6178'
+});
+
+// Verifica a conexão
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('Conectado ao banco de dados MySQL!');
+});
+
+// Cria uma rota para receber os dados do formulário
+app.post('/enviar-dados', function(req, res) {
+  const nome = req.body.nome;
+  const matricula = req.body.matricula;
+  const cpf = req.body.cpf;
+  const email = req.body.email;
+  const telefone = req.body.telefone;
+  const curso = req.body.curso;
+  const turma = req.body.turma;
+  const msg = req.body.msg;
+
+  // Insere os dados no banco de dados
+  const sql = "INSERT INTO dados (nome, matricula, cpf, email, telefone, curso, turma, informacoes) VALUES (?,?,?,?,?,?,?,?)";
+  connection.query(sql, [nome, matricula, cpf, email, telefone, curso, turma, msg], function(err, result) {
+    if (err) throw err;
+    console.log('Dados inseridos com sucesso!');
+    res.send('Dados inseridos com sucesso!');
+  });
+});
+
+// Inicia o servidor
+app.listen(port, function() {
+    console.log(`Servidor iniciado na porta ${port}`);
+  });
