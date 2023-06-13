7f4afe651d36023d22e1b22e47043987c577adff
João Fernandes Santos Filho <129438025+Fernandes072@users.noreply.github.com>
2023-06-13 16:43:09 -0300

    Add files via upload

--- testecadver.js
+++ testecadver.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
-const app = express();
+const port = process.env.PORT || 3000; // obter a porta do Vercel ou usar a porta 3000
 
-app.use(express.static('public'));
+const app = express();
 
 // Configura o middleware body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Cria a conexão com o banco de dados
 const connection = mysql.createConnection({
-    host: 'containers-us-west-190.railway.app',
-    user: 'root',
-    password: 'uF1YkyevPV5usJbw8iYn',
-    database: 'railway',
-    port: '6178'
+  host: 'containers-us-west-190.railway.app',
+  user: 'root',
+  password: 'uF1YkyevPV5usJbw8iYn',
+  database: 'railway',
+  port: '6178'
 });
 
 // Verifica a conexão
@@ -28,10 +28,15 @@ app.post('/enviar-dados', function(req, res) {
   const nome = req.body.nome;
   const matricula = req.body.matricula;
   const cpf = req.body.cpf;
+  const email = req.body.email;
+  const telefone = req.body.telefone;
+  const curso = req.body.curso;
+  const turma = req.body.turma;
+  const msg = req.body.msg;
 
   // Insere os dados no banco de dados
-  const sql = `INSERT INTO alunos (nome, matricula, cpf) VALUES (?, ?, ?)`;
-  connection.query(sql, [nome, matricula, cpf], function(err, result) {
+  const sql = "INSERT INTO dados (nome, matricula, cpf, email, telefone, curso, turma, informacoes) VALUES (?,?,?,?,?,?,?,?)";
+  connection.query(sql, [nome, matricula, cpf, email, telefone, curso, turma, msg], function(err, result) {
     if (err) throw err;
     console.log('Dados inseridos com sucesso!');
     res.send('Dados inseridos com sucesso!');
@@ -39,7 +44,6 @@ app.post('/enviar-dados', function(req, res) {
 });
 
 // Inicia o servidor
-const server = app.listen(process.env.PORT || 3000, function() {
-  const port = server.address().port;
+server.listen(port, () => {
   console.log(`Servidor iniciado na porta ${port}`);
 });
